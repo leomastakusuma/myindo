c9313e34a3cae1befc8e0b63a887d1670b0cb8cb
leomastakusuma <leomastakusuma@gmail.com>
2014-06-16 10:44:36 +0700

    fix menu

---
+++ public/menu.php
@@ -0,0 +1,12 @@
+<table>
+ <tr>
+    <ul>
+    <td>
+    	 <li><a href="<?php echo $this->url(array('controller'=>'Jabatan','action'=>'index'))?>">Jabatan</a></li>
+    </td>
+    <td>
+    	<li><a href="<?php echo $this->url(array('controller'=>'Departemen','action'=>'index'))?>">Departemen</a></li>
+    </td>
+    </ul>
+ </tr>
+</table>
