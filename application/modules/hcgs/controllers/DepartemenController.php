e348c4bf6c6278c46d8129383ee5d0e27df034ae
leomastakusuma <leomastakusuma@gmail.com>
2014-06-16 10:02:38 +0700

    versi 2

---
+++ application/modules/hcgs/controllers/DepartemenController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Hcgs_DepartemenController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $departemen = new Hcgs_Model_DbTable_Departemen();
+        $this->view->departemen = $departemen->getdepartemen();
+    }
+
+
+}
+
--- application/modules/hcgs/models/DbTable/Departemen.php
+++ application/modules/hcgs/models/DbTable/Departemen.php
@@ -3,10 +3,18 @@
 class Hcgs_Model_DbTable_Departemen extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'T_departemen';
+    protected $_name = 'T_Departemen';
 
 
-    
+    public function getdepartemen($order ='id_departemen')
+    {
+        $query = $this->select();
+        $query->from(($this->_name),array('*'));
+        $query->order($order,'asc');
+        $getdepartemen = $this->fetchAll($query);
+        
+        return $getdepartemen;
+    }
     
     
     
