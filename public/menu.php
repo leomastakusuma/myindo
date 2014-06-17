b7c638bb132c79d13b3321b3ab51ef6abb84f931
leomastakusuma@gmail.com <leomastakusuma@gmail.com>
2014-06-17 16:37:18 +0700

    Versi 01

--- public/menu.php
+++ public/menu.php
@@ -1,12 +1,38 @@
-<table>
+<table align="left" width="100%">
  <tr>
-    <ul>
+    
+    <td>|</td>
     <td>
-    	 <li><a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Jabatan','action'=>'index'))?>">Jabatan</a></li>
+    	 <a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Karyawan','action'=>'index'))?>">Karyawan </a>
     </td>
+    <td>|</td>
+    
     <td>
-    	<li><a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Departemen','action'=>'index'))?>">Departemen</a></li>
+    	 <a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Jabatan','action'=>'index'))?>">Jabatan </a>
     </td>
-    </ul>
+    <td>|</td>
+    <td>
+    	 <a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Golongan','action'=>'index'))?>">Golongan</a>
+    </td>
+    <td>|</td>
+    <td>
+    	<a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Departemen','action'=>'index'))?>">Departemen </a>
+    </td>
+    <td>|</td>   
+    <td>
+    	<a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Potongan','action'=>'index'))?>">Potongan </a>
+    </td>
+    <td>|</td>  
+     <td>
+    	<a href="<?php echo $this->url(array('module'=>'hcgs','controller'=>'Gaji','action'=>'index'))?>">Gaji </a>
+    </td>
+    <td>|</td>  
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    <td> </td>
+    
+    
  </tr>
 </table>
+
